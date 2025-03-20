2326f26355ce993612bd94fb6bd9ff999aecccfc
ferdouszihad <f.zihad333@gmail.com>
2025-03-20 20:30:09 +0600

    added basic navbar

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
